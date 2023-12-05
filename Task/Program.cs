354827131ae4d0ba40a05cf3a5e4e32c3dd37a68
Vyacheslav <bebenin.64@mail.ru>
2023-12-05 13:54:28 +0300

    Решил в С#

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] input_array = new string[6] {"0123", "arr", "hello", "world", "res", "zub"};
+string[] output_array = new string[input_array.Length];
+void ChangingArray(string[] input_array, string[] output_array)
+{
+    int count = 0;
+    for (int i = 0; i < input_array.Length; i++)
+    {
+    if(input_array[i].Length <= 3)
+        {
+        output_array[count] = input_array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ChangingArray(input_array, output_array);
+PrintArray(output_array);
